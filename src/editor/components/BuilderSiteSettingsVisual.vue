dace735afc6f4410854930ded39957ec82118725
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2018-11-28 17:47:40 +0300

    PTH-112 adds site settings -> visual settings

--- src/editor/components/BuilderSiteSettingsVisual.vue
+++ src/editor/components/BuilderSiteSettingsVisual.vue
@@ -3,10 +3,16 @@
     <div class="b-builder-site-settings-visual">
       <base-heading level="2">Visual</base-heading>
     </div>
+
+    <div slot="controls">
+      <BaseButton color="gray" @click="$emit('requestClose')">Cancel</BaseButton>
+      <BaseButton @click="applySettings">Save</BaseButton>
+    </div>
   </builder-modal-content-layout>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 import BuilderModalContentLayout from './BuilderModalContentLayout'
 
 export default {
@@ -14,6 +20,41 @@ export default {
 
   components: {
     BuilderModalContentLayout
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapState(['currentLanding'])
+  },
+
+  watch: {
+    currentLanding () {
+      this.updateSettings()
+    }
+  },
+
+  mounted () {
+    this.updateSettings()
+  },
+
+  methods: {
+    ...mapActions([
+      'storeSettings'
+    ]),
+    updateSettings () {
+      // const settings = this.currentLanding.settings
+    },
+    applySettings () {
+      const data = {
+      }
+
+      this.storeSettings(data)
+      this.$emit('requestClose')
+    }
   }
 }
 </script>
