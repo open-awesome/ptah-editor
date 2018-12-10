e85f300d66109f5659f3ea0f527942940ef74312
Alex Shabalin <temarie@ya.ru>
2018-12-10 12:03:14 +0500

    del svg attr

--- src/components/base/icons/IconGroupTop.vue
+++ src/components/base/icons/IconGroupTop.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="14" height="15" viewBox="0 0 14 15">
     <rect width="1" height="14" rx="0.5" transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 14 1)"/>
     <rect x="11" y="15" width="3" height="12" rx="1" transform="rotate(-180 11 15)"/>
     <rect x="6" y="11" width="3" height="8" rx="1" transform="rotate(-180 6 11)"/>
