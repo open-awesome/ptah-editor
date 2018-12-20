908762f10cd8646a90ab2270ed15ff2c42888114
Boris Butenko <iamborisbutenko@gmail.com>
2018-12-20 12:53:06 +0400

    PTH-164 #time 2h #comment fine

--- src/editor/components/VuseBuilder.vue
+++ src/editor/components/VuseBuilder.vue
@@ -11,7 +11,12 @@
         'is-editable': $builder.isEditing,
         'fp-scroll': currentLanding.settings.fullPageScroll === 'yes'
       }">
-      <component v-for="section in $builder.sections" :is="section.name" :key="section.id" :id="section.id"></component>
+      <component
+          v-for="section in $builder.sections"
+          :key="section.id"
+          :is="section.name"
+          :id="section.id"
+          @click.native="selectSidebarSection(section)"/>
       <div class="controller-intro" v-if="emptySections">
         <h3>&larr; Choose layout from the menu</h3>
       </div>
@@ -195,6 +200,16 @@ export default {
       }
       node.innerHTML = `<source src="${settings.video}" type="video/mp4"></source>`
       this.$refs.artboard.appendChild(node)
+    },
+
+    async selectSidebarSection (section) {
+      let menuItem = document.getElementById(`menu-item-${section.id}`)
+      if (!menuItem) {
+        return
+      }
+      // --- coz directive v-scroll-to is called
+      await this.$nextTick()
+      menuItem.click()
     }
   }
 }
