d0a90fe914cae6b5d2b1c2f7f280adfa8bf14fff
Boris Butenko <iamborisbutenko@gmail.com>
2019-01-17 15:51:30 +0400

    PTH-213 #time 10m #comment fixes icons directions

--- src/editor/components/BuilderSettingsSlots.vue
+++ src/editor/components/BuilderSettingsSlots.vue
@@ -33,13 +33,13 @@
 
       <ul v-show="isRowDir">
         <li :class="{ active: align === 'flex-start' }" @click="changeAlign('flex-start')">
-          <icon-base name="groupBottom"/>
+          <icon-base name="groupTop"/>
         </li>
         <li :class="{ active: align === 'center' }" @click="changeAlign('center')">
           <icon-base name="groupCenterVertical"/>
         </li>
         <li :class="{ active: align === 'flex-end' }" @click="changeAlign('flex-end')">
-          <icon-base name="groupTop"/>
+          <icon-base name="groupBottom"/>
         </li>
       </ul>
 
