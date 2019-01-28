c8ae28373cfae03affc670b16da287752bf8422c
Aleksandr Grebnev <grebnev.av@gmail.com>
2019-01-28 19:39:07 +0300

    PTH-237 change 'vue2-scrollbar' on 'vuebar'

--- src/components/base/BaseScrollContainer.vue
+++ src/components/base/BaseScrollContainer.vue
@@ -1,10 +1,12 @@
 <script>
 import VueScrollbar from 'vue2-scrollbar'
+import Vuebar from 'vuebar'
 require('vue2-scrollbar/dist/style/vue2-scrollbar.css')
 
 export default {
   components: {
-    VueScrollbar
+    VueScrollbar,
+    Vuebar
   },
   props: {
     styling: {
@@ -25,9 +27,14 @@ export default {
 </script>
 
 <template>
-  <vue-scrollbar class="b-scroll-bar" :style="[styling, cssProps]">
-    <slot></slot>
-  </vue-scrollbar>
+  <div
+    class="vuebar-element"
+    v-bar="{ preventParentScroll: true, scrollThrottle: 30 }"
+    :style="[styling, cssProps]">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <style lang="sass">
@@ -47,4 +54,47 @@ export default {
 .b-scroll-bar
   & .scrollbar
     background-color: var(--bg-color)
+
+.vuebar-element
+  height: 100%
+  width: 100%
+
+.vb > .vb-dragger
+  z-index: 5
+  width: 1rem
+  right: 0
+
+.vb > .vb-dragger > .vb-dragger-styler
+  -webkit-backface-visibility: hidden
+  backface-visibility: hidden
+  -webkit-transform: rotate3d(0,0,0,0)
+  transform: rotate3d(0,0,0,0)
+  -webkit-transition: background-color 100ms ease-out, margin 100ms ease-out, height 100ms ease-out;
+  transition: background-color 100ms ease-out, margin 100ms ease-out, height 100ms ease-out;
+  background-color: var(--bg-color)
+  opacity: 0.5
+  margin: 5px 5px 5px 0
+  border-radius: 20px
+  height: calc(100% - 10px)
+  display: block
+
+.vb.vb-scrolling-phantom > .vb-dragger > .vb-dragger-styler
+  background-color: var(--bg-color)
+  opacity: 0.3
+
+.vb > .vb-dragger:hover > .vb-dragger-styler
+  background-color: var(--bg-color)
+  margin: 0px
+  height: 100%
+  opacity: 0.5
+
+.vb.vb-dragging > .vb-dragger > .vb-dragger-styler
+  background-color: var(--bg-color)
+  margin: 0px
+  height: 100%
+  opacity: 0.5
+
+.vb.vb-dragging-phantom > .vb-dragger > .vb-dragger-styler
+  background-color: var(--bg-color)
+  opacity: 0.5
 </style>
