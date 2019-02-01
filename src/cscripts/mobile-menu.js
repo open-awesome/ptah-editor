d4487039d478ef8706f96c03ac599d435bb11f42
borisbutenko <iamborisbutenko@gmail.com>
2019-02-01 12:12:34 +0400

    Header1 section variant as GOG.com fine PTH-246 #time 4h

---
+++ src/cscripts/mobile-menu.js
@@ -0,0 +1,34 @@
+const habmurger = document.getElementById('js-hamburger')
+let menu = null
+
+export function initMobileMenu () {
+  if (!habmurger) {
+    return
+  }
+  habmurger.addEventListener('click', toggleMobileMenu, false)
+  document.body.addEventListener('click', closeMobileMenu, false)
+  document.body.style.paddingTop = '80px'
+}
+
+function toggleMobileMenu (e) {
+  menu = document.querySelector(this.dataset.target)
+
+  if (!menu) {
+    return
+  }
+
+  this.classList.toggle('is-active')
+  menu.classList.toggle('is-active')
+}
+
+function closeMobileMenu ({ target }) {
+  if (target.closest('#js-hamburger') || target.closest('[id^="mobile-menu"]')) {
+    return
+  }
+  if (menu) {
+    menu.classList.remove('is-active')
+  }
+  habmurger.classList.remove('is-active')
+}
+
+void initMobileMenu()
