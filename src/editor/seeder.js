f110ca12c8ea59ed8f764bf7973c7d667a8179da
Aleksandr Grebnev <grebnev.av@gmail.com>
2019-04-02 10:23:36 +0400

    fix, PTH-386

--- src/editor/seeder.js
+++ src/editor/seeder.js
@@ -665,7 +665,7 @@ const data = new Map([
       table: {
         head: {
           'font-name': 'Lato',
-          'font-size': '14px',
+          'font-size': '1.4rem',
           'font-weight': 'normal',
           'font-style': 'normal',
           'text-decoration': 'none',
@@ -674,7 +674,7 @@ const data = new Map([
         },
         body: {
           'font-family': 'Lato',
-          'font-size': '14px',
+          'font-size': '1.4rem',
           'font-weight': 'normal',
           'font-style': 'normal',
           'text-decoration': 'none',
