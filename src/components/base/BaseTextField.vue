db2aed30a4b95036a8cc8c78ca6cca675756fa05
Temarie <temarie@ya.ru>
2019-04-24 09:30:28 +0500

    [PTH-444] control section height

--- src/components/base/BaseTextField.vue
+++ src/components/base/BaseTextField.vue
@@ -34,7 +34,7 @@ export default {
 
   props: {
     value: {
-      type: String,
+      type: [String, Number],
       default: ''
     },
     hasError: {
---
+++ src/editor/components/controls/TheControlSectionHeight.vue
@@ -0,0 +1,125 @@
+<script>
+import { mapState, mapActions } from 'vuex'
+import * as _ from 'lodash-es'
+
+export default {
+  data () {
+    return {
+      heightValueTypesList: [
+        {
+          text: 'auto',
+          value: 'auto'
+        },
+        {
+          text: 'Viewport Height',
+          value: 'vh'
+        },
+        {
+          text: 'Pixels',
+          value: 'px'
+        }
+      ],
+      heigthValueType: ''
+    }
+  },
+
+  computed: {
+    ...mapState('Sidebar', [
+      'settingObjectOptions'
+    ]),
+
+    vhValue: {
+      get () {
+        let h = parseInt(this.settingObjectOptions.height) || 100
+        return h > 100 ? h : 100
+      },
+
+      set (value) {
+        let styles = { height: value + 'vh' }
+        this.updateSettingOptions(_.merge({}, this.settingObjectOptions, { styles }))
+      }
+    },
+
+    pxValue: {
+      get () {
+        let h = parseInt(this.settingObjectOptions.height) || 500
+        if (this.settingObjectOptions.height.indexOf('rem') > 0) {
+          h = h * 10
+        }
+        return h
+      },
+
+      set (value) {
+        let styles = { height: value + 'px' }
+        this.updateSettingOptions(_.merge({}, this.settingObjectOptions, { styles }))
+      }
+    }
+  },
+
+  created () {
+    if (this.settingObjectOptions.height && this.settingObjectOptions.height.indexOf('px') > 0) {
+      this.heigthValueType = 'px'
+    } else if (this.settingObjectOptions.height && this.settingObjectOptions.height.indexOf('vh') > 0) {
+      this.heigthValueType = 'vh'
+    } else {
+      this.heigthValueType = 'auto'
+    }
+  },
+
+  methods: {
+    ...mapActions('Sidebar', [
+      'updateSettingOptions'
+    ]),
+
+    onTypeChange () {
+      if (this.heigthValueType === 'auto') {
+        let styles = { height: 'auto' }
+        this.updateSettingOptions(_.merge({}, this.settingObjectOptions, { styles }))
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="control-height">
+    <h6>Section height</h6>
+    <BaseRadioCheck :list="heightValueTypesList" v-model="heigthValueType" @change="onTypeChange"/>
+
+    <div class="control-height__input" v-if="heigthValueType === 'vh'">
+      <!-- slider -->
+      <p>1 vh is equivalent To 1% of the height of the browser window</p>
+      <base-range-slider
+        v-model="vhValue"
+         step="1" min="1" max="100">
+        {{vhValue}} vh
+      </base-range-slider>
+    </div>
+
+    <div class="control-height__input" v-if="heigthValueType === 'px'">
+      <!-- text field -->
+      <base-text-field v-model="pxValue" label="Height in pixels"></base-text-field>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.control-height
+  padding: 0 0 1rem
+
+  &__input
+    margin: 2rem 0 0
+
+    p
+      color: $grey-middle
+
+  h6
+    min-width: 28rem
+    margin: 0 0 2.8rem 0
+    padding: 0
+    color: #272727
+    font-size: 1.6rem
+    font-weight: bold
+    &:first-letter
+      text-transform: uppercase
+</style>
