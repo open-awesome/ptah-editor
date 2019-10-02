ebf76b2660165b0bd6ba68f067835b28c8fd74e2
Aleksandr Grebnev <grebnev.av@gmail.com>
2019-10-02 17:20:42 +0400

    background of section in mobile, #192079

--- src/components/mixins/sectionMedia.js
+++ src/components/mixins/sectionMedia.js
@@ -14,22 +14,31 @@ export default {
 
   methods: {
     mediaStyles () {
-      this.$sectionData.objVarsMedia = {
-        '--mobile-section-margin-top': this.$sectionData.mainStyle.media['is-mobile']['margin-top'],
-        '--mobile-section-margin-right': this.$sectionData.mainStyle.media['is-mobile']['margin-right'],
-        '--mobile-section-margin-bottom': this.$sectionData.mainStyle.media['is-mobile']['margin-bottom'],
-        '--mobile-section-margin-left': this.$sectionData.mainStyle.media['is-mobile']['margin-left'],
-        '--mobile-section-padding-top': this.$sectionData.mainStyle.media['is-mobile']['padding-top'],
-        '--mobile-section-padding-right': this.$sectionData.mainStyle.media['is-mobile']['padding-right'],
-        '--mobile-section-padding-bottom': this.$sectionData.mainStyle.media['is-mobile']['padding-bottom'],
-        '--mobile-section-padding-left': this.$sectionData.mainStyle.media['is-mobile']['padding-left'],
-        '--mobile-section-background-color': this.$sectionData.mainStyle.media['is-mobile']['background-color'],
-        '--mobile-section-background-image': this.$sectionData.mainStyle.media['is-mobile']['background-image'],
-        '--mobile-section-background-position': this.$sectionData.mainStyle.media['is-mobile']['background-position'],
-        '--mobile-section-background-repeat': this.$sectionData.mainStyle.media['is-mobile']['background-repeat'],
-        '--mobile-section-background-size': this.$sectionData.mainStyle.media['is-mobile']['background-size'],
-        '--mobile-section-background-attachment': this.$sectionData.mainStyle.media['is-mobile']['background-attachment']
-      }
+      let obj = {}
+      let list = [
+        'margin-top',
+        'margin-right',
+        'margin-bottom',
+        'margin-left',
+        'padding-top',
+        'padding-right',
+        'padding-bottom',
+        'padding-left',
+        'background-color',
+        'background-image',
+        'background-position',
+        'background-repeat',
+        'background-size',
+        'background-attachment'
+      ]
+
+      list.forEach((e) => {
+        obj[`--mobile-section-${e}`] = this.$sectionData.mainStyle.media['is-mobile'][e]
+          ? this.$sectionData.mainStyle.media['is-mobile'][e]
+          : this.$sectionData.mainStyle.styles[e]
+      })
+
+      this.$sectionData.objVarsMedia = obj
     }
   }
 }
