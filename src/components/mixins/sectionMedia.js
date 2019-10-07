c77e27c4954831fcbd29408b2a8189eb941ec24b
Aleksandr Grebnev <grebnev.av@gmail.com>
2019-10-07 11:35:53 +0400

    fix refactoring

--- src/components/mixins/sectionMedia.js
+++ src/components/mixins/sectionMedia.js
@@ -1,3 +1,29 @@
+const LIST_PROPS_STYLES = [
+  'margin-top',
+  'margin-right',
+  'margin-bottom',
+  'margin-left',
+  'padding-top',
+  'padding-right',
+  'padding-bottom',
+  'padding-left',
+  'background-color',
+  'background-image',
+  'background-position',
+  'background-repeat',
+  'background-size',
+  'background-attachment'
+]
+let LIST_PROPS_TYPO = [
+  'font-family',
+  'font-size',
+  'color',
+  'line-height'
+]
+let LIST_PROPS_SIZE = [
+  'width'
+]
+
 export default {
   $schema: {
     objVarsMedia: {},
@@ -7,9 +33,27 @@ export default {
   },
 
   watch: {
-    '$sectionData.mainStyle': {
+    '$sectionData.mainStyle.styles': {
       handler () {
         this.mediaStyles()
+      },
+      deep: true
+    },
+    '$sectionData.mainStyle.textStyles': {
+      handler () {
+        this.mediaTextStyles()
+      },
+      deep: true
+    },
+    '$sectionData.mainStyle.sizeIcons': {
+      handler () {
+        this.mediaSizeIcons()
+      },
+      deep: true
+    },
+    '$sectionData.mainStyle.table': {
+      handler () {
+        this.mediaTableStyles()
       },
       deep: true
     }
@@ -18,78 +62,62 @@ export default {
   methods: {
     mediaStyles () {
       let obj = {}
-      let objTypo = {}
-      let objSizeIcons = {}
-      let objTable = {}
-      let list = [
-        'margin-top',
-        'margin-right',
-        'margin-bottom',
-        'margin-left',
-        'padding-top',
-        'padding-right',
-        'padding-bottom',
-        'padding-left',
-        'background-color',
-        'background-image',
-        'background-position',
-        'background-repeat',
-        'background-size',
-        'background-attachment'
-      ]
-      let listTypo = [
-        'font-family',
-        'font-size',
-        'color',
-        'line-height'
-      ]
-      let listSize = [
-        'width'
-      ]
 
-      list.forEach((e) => {
+      LIST_PROPS_STYLES.forEach((e) => {
         obj[`--mobile-section-${e}`] = this.$sectionData.mainStyle.media['is-mobile'][e]
           ? this.$sectionData.mainStyle.media['is-mobile'][e]
           : this.$sectionData.mainStyle.styles[e]
       })
       this.$sectionData.objVarsMedia = obj
+    },
+
+    mediaTextStyles () {
+      let obj = {}
 
       // for section gallery
       if (this.$sectionData.mainStyle['textStyles']) {
-        listTypo.forEach((e) => {
-          objTypo[`--mobile-section-text-styles-chapter-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['textStyles']['chapter'][e]
+        LIST_PROPS_TYPO.forEach((e) => {
+          obj[`--mobile-section-text-styles-chapter-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['textStyles']['chapter'][e]
             ? this.$sectionData.mainStyle.media['is-mobile']['textStyles']['chapter'][e]
             : this.$sectionData.mainStyle['textStyles']['chapter'][e]
 
-          objTypo[`--mobile-section-text-styles-text-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['textStyles']['text'][e]
+          obj[`--mobile-section-text-styles-text-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['textStyles']['text'][e]
             ? this.$sectionData.mainStyle.media['is-mobile']['textStyles']['text'][e]
             : this.$sectionData.mainStyle['textStyles']['text'][e]
         })
-        this.$sectionData.objVarsTypo = objTypo
+        this.$sectionData.objVarsTypo = obj
       }
+    },
+
+    mediaSizeIcons () {
+      let obj = {}
 
       // for section system
       if (this.$sectionData.mainStyle['sizeIcons']) {
-        listSize.forEach((e) => {
-          objSizeIcons[`--mobile-section-size-icons-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['sizeIcons'][e]
+        LIST_PROPS_SIZE.forEach((e) => {
+          obj[`--mobile-section-size-icons-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['sizeIcons'][e]
             ? this.$sectionData.mainStyle.media['is-mobile']['sizeIcons'][e] + 'px'
             : this.$sectionData.mainStyle['sizeIcons'][e] + 'px'
         })
-        this.$sectionData.objVarsSizeIcons = objSizeIcons
+        this.$sectionData.objVarsSizeIcons = obj
       }
+    },
+
+    mediaTableStyles () {
+      let obj = {}
 
       // for table
       if (this.$sectionData.mainStyle['table']) {
-        listTypo.forEach((e) => {
-          objTable[`--mobile-section-table-head-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['table']['head'][e]
+        LIST_PROPS_TYPO.forEach((e) => {
+          obj[`--mobile-section-table-head-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['table']['head'][e]
             ? this.$sectionData.mainStyle.media['is-mobile']['table']['head'][e]
             : this.$sectionData.mainStyle['table']['head'][e]
 
-          objTable[`--mobile-section-table-body-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['table']['body'][e]
+          obj[`--mobile-section-table-body-${e}`] = this.$sectionData.mainStyle.media['is-mobile']['table']['body'][e]
             ? this.$sectionData.mainStyle.media['is-mobile']['table']['body'][e]
             : this.$sectionData.mainStyle['table']['body'][e]
         })
-        this.$sectionData.objVarsTable = objTable
+        this.$sectionData.objVarsTable = obj
       }
     }
   }
